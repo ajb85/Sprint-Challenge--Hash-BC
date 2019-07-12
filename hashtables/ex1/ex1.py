06f2127b322272c416c3107a2b5b14fd1aaa063d
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-07-12 12:42:12 -0400

    Solves ht1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,23 +1,27 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-
-    """
-    YOUR CODE HERE
-    """
-
+    for i in range(length): # O(n)
+        weight = weights[i]
+        pair_weight = limit - weight
+        pair_index = hash_table_retrieve(ht, pair_weight)
+        if(pair_index != None):
+            pairs = (i, pair_index) if i > pair_index else (pair_index, i)
+            print_answer(pairs)
+            return pairs
+        hash_table_insert(ht, weight, i)
     return None
 
 
 def print_answer(answer):
-    if answer is None:
-        print(str(answer[0] + " " + answer[1]))
+    if answer is not None:
+        print(str(answer[0]) + " " + str(answer[1]))
     else:
         print("None")
+
+get_indices_of_item_weights([ 4, 6, 10, 15, 16 ], 5, 21)
